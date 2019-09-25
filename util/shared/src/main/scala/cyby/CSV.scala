1846e794f4834d950976cb28be2d4f28c93a2b4d
stefan-hoeck <hock@zhaw.ch>
2019-09-25 16:32:37 +0200

    minor changes to CSV codecs

--- util/shared/src/main/scala/cyby/CSV.scala
+++ util/shared/src/main/scala/cyby/CSV.scala
@@ -44,36 +44,87 @@ object CsvEncoder {
   * possibility of failure)
   */
 trait CsvDecoder[A] {
-  def apply(ts: List[String]): ValNel[String,A]
+  self ⇒
+  final def apply(ts: List[String]): ValNel[CsvError,A] = run(None, 0, ts)
 
-  def map[B](f: A ⇒ B): CsvDecoder[B] = CsvDecoder.inst(apply(_) map f)
+  final def runRow(row: Int, ts: List[String]): ValNel[CsvError,A] = run(Some(row),0,ts)
+
+  protected def run(row: Option[Int], col: Int, ts: List[String]): ValNel[CsvError,A]
+
+  def map[B](f: A ⇒ B): CsvDecoder[B] = new CsvDecoder[B]{
+    protected def run(row: Option[Int], col: Int, ts: List[String]) =
+      self.run(row, col, ts) map f
+  }
 }
 
 object CsvDecoder {
+  import CsvError._
+
   def apply[A](implicit A: CsvDecoder[A]): CsvDecoder[A] = A
 
   def derive[A,HL](implicit A: CsvDecoder[HL], G: Generic.Aux[A,HL])
     : CsvDecoder[A] = A map G.from
 
-  def inst[A](dec: List[String] ⇒ ValNel[String,A]): CsvDecoder[A] =
-    new CsvDecoder[A]{
-      def apply(ts: List[String]) = dec(ts)
-    }
-
-  def decode[A](ts: List[String])(implicit A: CsvDecoder[A]): ValNel[String,A] =
+  def decode[A](ts: List[String])(implicit A: CsvDecoder[A]): ValNel[CsvError,A] =
     A apply ts
 
-  def uncsv[A:CsvDecoder](s: String, sep: String): ValNel[String,A] =
-    decode[A](s split sep toList)
+  def decodeRow[A](row: Int, ts: List[String])(implicit A: CsvDecoder[A]): ValNel[CsvError,A] =
+    A.runRow(row, ts)
 
-  implicit lazy val hnilI: CsvDecoder[HNil] = inst{
-    case Nil ⇒ valid(HNil)
-    case t   ⇒ fail(s"Expected EOI: $t")
+  def uncsv[A:CsvDecoder](s: String, sep: Char): ValNel[CsvError,A] =
+    decode[A](split(s,sep))
+
+  def uncsvRow[A:CsvDecoder](row: Int, sep: Char)(s: String): ValNel[CsvError,A] =
+    CsvDecoder[A].runRow(row, split(s,sep))
+
+  private def split(s: String, sep: Char): List[String] = s indexOf sep match {
+    case -1 ⇒ List(s)
+    case i  ⇒ {
+      val (h,t) = s splitAt i
+      h :: split(t.tail, sep)
+    }
   }
 
-  implicit def hconsI[H:Read,T<:HList:CsvDecoder]: CsvDecoder[H :+: T] =
-    inst{
-      case Nil ⇒ fail(s"Unexpected end of input")
-      case h::t ⇒ (Read[H] readVS h, decode[T](t)).mapN(_ :: _)
+  implicit lazy val hnilI: CsvDecoder[HNil] = new CsvDecoder[HNil]{
+    protected def run(row: Option[Int], col: Int, ts: List[String]) = ts match {
+      case Nil ⇒ valid(HNil)
+      case t   ⇒ fail(NoEOIErr(row, col).e)
     }
+  }
+
+  implicit def hconsI[H,T<:HList](implicit H: Read[H], T: CsvDecoder[T])
+    : CsvDecoder[H :+: T] = new CsvDecoder[H :+: T] {
+    protected def run(row: Option[Int], col: Int, ts: List[String]) = ts match {
+      case Nil  ⇒ fail(EOIErr(row).e)
+      case h::t ⇒ (
+        H.readV(h, ColErr(row, col, H msg h)),
+        T.run(row, col+1, t)
+      ).mapN(_ :: _)
+    }
+  }
 }
+
+sealed trait CsvError {
+  def e: CsvError = this
+  val row: Option[Int]
+
+  override def toString: String = {
+    def rowPrefix = row.fold("")(i ⇒ s"Row ${i}: ")
+    def colStr = this match {
+      case CsvError.ColErr(_, c, m) ⇒ s"Column ${c}: ${m}"
+      case CsvError.EOIErr(_)       ⇒ "Unexpected end of line"
+      case CsvError.NoEOIErr(_,c)   ⇒ s"Column ${c}: expected end of line"
+    }
+
+    rowPrefix ++ colStr
+  }
+}
+
+object CsvError {
+  case class ColErr(row: Option[Int], col: Int, msg: String) extends CsvError
+
+  case class EOIErr(row: Option[Int]) extends CsvError
+
+  case class NoEOIErr(row: Option[Int], col: Int) extends CsvError
+}
+
